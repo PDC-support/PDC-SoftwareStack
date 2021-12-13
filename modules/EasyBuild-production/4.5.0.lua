3d4bde9019ba28359b3900401fd4daafe0b7c5f4
Henric Zazzi <hzazzi@kth.se>
2021-12-13 10:29:24 +0100

    fixed bug in easybuild

--- modules/EasyBuild-production/4.5.0.lua
+++ modules/EasyBuild-production/4.5.0.lua
@@ -18,7 +18,7 @@ whatis([==[Homepage: https://easybuild.io/]==])
 -- Main path for external repositories, for EasyBuild installation path and for EasyBuild
 local cpe = "/pdc/software/" ..os.getenv("CRAY_PE_VERSION") .. "/"
 local repos = "/pdc/software/eb_repo/"
-local root = cpe .. "other/easybuild/4.4.2/"
+local root = cpe .. "other/easybuild/4.5.0/"
 local user = os.getenv("USER")
 local user_dir = string.sub(user, 1, 1) .. "/" .. user .. "/"
 
