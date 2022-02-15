f6b758b4b3e14bdd7dc19d5478488fc26bfcd288
Henric Zazzi <hzazzi@kth.se>
2022-02-15 11:16:19 +0100

    added systemdefault

---
+++ modules/systemdefault/1.0.0.lua
@@ -0,0 +1,15 @@
+help([==[
+
+Description
+===========
+Custom system Environment module for PDC
+
+More information
+================
+ - Homepage: https://www.pdc.kth.se/
+]==])
+
+whatis([==[Description: 
+    This is an environment module for custom applications and scripts at PDC
+]==])
+set_alias("projinfo","/afs/pdc.kth.se/misc/pdc/adm/Allocation/plasma/projinfo.py")
