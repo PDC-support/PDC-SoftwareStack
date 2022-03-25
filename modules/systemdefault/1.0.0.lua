95660240ff649a288395f2add352292cd122862a
Henric Zazzi <hzazzi@kth.se>
2022-03-25 07:34:11 +0100

    changed the link to projinfo

--- modules/systemdefault/1.0.0.lua
+++ modules/systemdefault/1.0.0.lua
@@ -13,5 +13,5 @@ whatis([==[Description:
     This is an environment module for custom applications and scripts at PDC
 ]==])
 add_property("lmod","sticky")
-set_alias("projinfo","/afs/pdc.kth.se/misc/pdc/adm/Allocation/plasma/projinfo.py")
 prepend_path("PATH","/pdc/software/modules/systemdefault/bin")
+prepend_path("PATH","/afs/pdc.kth.se/misc/pdc/adm/Allocation/plasma")
