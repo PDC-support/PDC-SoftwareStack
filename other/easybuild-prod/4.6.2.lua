d6e21fd890b26f91724e830adc0b93446ab5df96
Henric Zazzi <hzazzi@kth.se>
2022-11-11 15:52:17 +0100

    added new easybuild

--- other/easybuild-prod/4.6.2.lua
+++ other/easybuild-prod/4.6.2.lua
@@ -2,7 +2,7 @@ help([==[
 
 Description
 ===========
-Local user EasyBuild module file.
+Global EasyBuild module file.
 
 
 More information
@@ -16,14 +16,14 @@ whatis([==[Description:
 whatis([==[Homepage: https://easybuild.io/]==])
 
 -- Main path for external repositories, for EasyBuild installation path and for EasyBuild
-local cpe = "/pdc/software/" .. os.getenv("CRAY_PE_VERSION") .. "/"
+local cpe = "/pdc/software/" ..os.getenv("CRAY_PE_VERSION") .. "/"
 local repos = "/pdc/software/eb_repo/"
 local root = cpe .. "other/easybuild/4.6.2/"
-local local_user = os.getenv("HOME") .. "/.local/easybuild/"
 
 -- Local paths
 local lumi_software = repos .. "LUMI-SoftwareStack/easybuild/"
 local pdc_software = repos .. "PDC-SoftwareStack/easybuild/"
+local archive = repos .. "archive"
 
 prepend_path("PATH", pathJoin(root, "bin"))
 prepend_path("PYTHONPATH", pathJoin(root, "lib/python3.9/site-packages/"))
@@ -31,7 +31,7 @@ append_path("EASYBUILD_ROBOT_PATHS", pathJoin(pdc_software, "easyconfigs"))
 append_path("EASYBUILD_ROBOT_PATHS", pathJoin(lumi_software, "easyconfigs"))
 append_path("EASYBUILD_ROBOT_PATHS", pathJoin(repos, "LUMI-EasyBuild-contrib/easybuild/easyconfigs"))
 append_path("EASYBUILD_ROBOT_PATHS", pathJoin(repos, "CSCS-production/easybuild/easyconfigs"))
-setenv("EASYBUILD_REPOSITORYPATH", repos)
+setenv("EASYBUILD_REPOSITORYPATH", archive)
 setenv("EASYBUILD_INCLUDE_EASYBLOCKS", lumi_software .. "easyblocks/*/*.py")
 setenv("EASYBUILD_INCLUDE_TOOLCHAINS", lumi_software .. "toolchains/*.py," .. lumi_software .. "toolchains/compiler/*.py")
 setenv("EB_PYTHON", "python3")
@@ -42,7 +42,6 @@ setenv("EASYBUILD_MODULE_DEPENDS_ON", "False")
 setenv("EASYBUILD_MODULE_EXTENSIONS", "True")
 setenv("EASYBUILD_PARALLEL","64")
 setenv("EASYBUILD_RECURSIVE_MODULE_UNLOAD", "False")
-setenv("EASYBUILD_INSTALLPATH", local_user)
-setenv("EASYBUILD_SOURCEPATH", pathJoin(local_user, "sources"))
-append_path("MODULEPATH", pathJoin(local_user, "modules/all/"))
+setenv("EASYBUILD_INSTALLPATH", pathJoin(cpe, "eb"))
+setenv("EASYBUILD_SOURCEPATH",  pathJoin(cpe, "eb/sources"))
 setenv("EASYBUILD_BUILDPATH", pathJoin("/tmp", os.getenv("USER")))
