5fbbf60e9f8bb4caeef4b5dc4af97423e4c4eb1b
Henric Zazzi <hzazzi@kth.se>
2023-04-04 18:01:57 +0200

    added 22.06 module

---
+++ modules/PDC/22.06.lua
@@ -0,0 +1,22 @@
+help([==[
+
+Description
+===========
+Cray Programming Environment 22.06
+]==])
+
+whatis([==[Description: 
+    CPE for user to built software
+]==])
+
+local cpe_version = "22.06"
+family("cpe")
+
+conflict("PDC")
+local root = "/pdc/software"
+
+setenv("CRAY_PE_VERSION", cpe_version)
+prepend_path("MODULEPATH", pathJoin(root, "PDC", cpe_version, "eb"))
+prepend_path("MODULEPATH", pathJoin(root, "PDC", cpe_version, "other"))
+prepend_path("MODULEPATH", pathJoin(root, "PDC", cpe_version, "spack"))
+load("cpe/" .. cpe_version)
