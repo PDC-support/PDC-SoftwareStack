0a5812f1939dcd08e066971008555e1bb26b8898
Henric Zazzi <hzazzi@kth.se>
2023-04-05 07:29:36 +0200

    added variables for PDC and reframe

--- modules/systemdefault/1.0.0.lua
+++ modules/systemdefault/1.0.0.lua
@@ -15,4 +15,19 @@ whatis([==[Description:
 add_property("lmod","sticky")
 prepend_path("PATH","/pdc/software/modules/systemdefault/bin")
 append_path("PATH","/pdc/software/tools")
-append_path("PDC_REFRAME", "/pdc/software/resources/reframe/")
+
+-- Local paths
+local cfs = "/cfs/klemming/"
+local user = os.getenv("USER")
+local home = os.getenv("HOME")
+local user_dir = string.sub(user, 1, 1) .. "/" .. user .. "/"
+
+if user ~= "root" then
+  setenv("PDC_BACKUP", home)
+  setenv("PDC_TMP", cfs .. "scratch/" .. user_dir)
+  end
+
+setenv("PDC_SITE", "pdc")
+setenv("PDC_RESOURCE", "dardel")
+setenv("PDC_REFRAME", "/pdc/software/resources/reframe")
+setenv("PDC_SHUB", "/pdc/software/resources/sing_hub")
